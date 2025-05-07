76196428843feea78e385657d002476a78012f7d
Damian Sire <damiansire@gmail.com>
2025-05-07 18:47:52 +0700

    Add example environment configuration for Firebase

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        projectId: 'your-project-id',
+        appId: 'your-app-id',
+        storageBucket: 'your-storage-bucket',
+        apiKey: 'your-api-key',
+        authDomain: 'your-auth-domain',
+        messagingSenderId: 'your-messaging-sender-id'
+    }
+}; 
